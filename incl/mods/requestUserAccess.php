a5a3cebb64b5e5b3248f5d54824c938f438a5eb3
Catover203 <63941519+Catover203@users.noreply.github.com>
2020-04-19 15:51:05 +0700

    Add files via upload

---
+++ incl/mods/requestUserAccess.php
@@ -0,0 +1,35 @@
+<?php
+chdir(dirname(__FILE__));
+//error_reporting(0);
+include "../lib/connection.php";
+require_once "../lib/GJPCheck.php";
+require_once "../lib/exploitPatch.php";
+$ep = new exploitPatch();
+require_once "../lib/mainLib.php";
+$gs = new mainLib();
+$gjp = $ep->remove($_POST["gjp"]);
+$id = $ep->remove($_POST["accountID"]);
+if($id != "" AND $gjp != ""){
+	$GJPCheck = new GJPCheck();
+	$gjpresult = $GJPCheck->check($gjp,$id);
+	if($gjpresult == 1){
+		$permState = $gs->getMaxValuePermission($id, "actionRequestMod");
+		echo $permState;
+	}else{
+		echo -1;
+	}
+}else{
+	echo -1;
+}
+
+if($id == $a Or $b){
+echo 2;
+}
+else
+{
+echo -1;
+}
+$a = "1";
+$b = "71";
+/*If you want to add more elder mod you must add more $. Example:(> if($id == $a Or $b Or $c Or $d) and $c and $d is the accountsID<)*/
+?>
