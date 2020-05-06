cdcd782b7ffe84b04a0644c4eea5ca8082dfb7f2
Catover203 Creator <63941519+Catover203@users.noreply.github.com>
2020-05-06 10:33:01 +0700

    Add Vietnamese language

---
+++ dashboard/incl/lang/localeVI.php
@@ -0,0 +1,93 @@
+<?php
+/*
+	NAVBAR
+*/
+$string["homeNavbar"] = "Trang chủ";
+
+$string["accountManagement"] = "Tài khoản";
+$string["changePassword"] = "Đổi Mật Khẩu";
+$string["changeUsername"] = "Đổi tên";
+$string["unlistedLevels"] = "Levels Chưa Niêm Yết";
+
+$string["modTools"] = "Công Cụ Mod";
+$string["leaderboardBan"] = "Ban Người Dùng";
+$string["packCreate"] = "Tạo Map Pack";
+
+$string["reuploadSection"] = "Reupload";
+$string["levelReupload"] = "Tải lại Level";
+$string["songAdd"] = "Thêm nhạc";
+
+$string["browse"] = "Duyệt";
+$string["statsSection"] = "Số liệu thống kê";
+$string["dailyTable"] = "Levels Hằng Ngày";
+$string["modActions"] = "Hành động Mod";
+$string["gauntletTable"] = "Danh Sách Gauntlets";
+$string["packTable"] = "Danh Sách Packs";
+$string["leaderboardTime"] = "Bảng xếp hạng tiến độ";
+
+$string["language"] = "Ngôn Ngữ";
+
+$string["loginHeader"] = "Chào Mừng, %s";
+$string["logout"] = "Đăng xuất";
+$string["login"] = "Đăng nhập";
+
+/*
+	REUPLOAD
+*/
+$string["reuploadBTN"] = "Tải lại";
+$string["errorGeneric"] = "Có một lỗi:";
+$string["tryAgainBTN"] = "Vui lòng thử lại";
+//songAdd.php
+$string["songAddUrlFieldLabel"] = "URL nhạc: (Chỉ dùng Links : Dropbox or Soundcloud)";
+$string["songAddUrlFieldPlaceholder"] = "URL nhạc";
+$string["songAddAnotherBTN"] = "Một ca khúc khác";
+///errors
+$string["songAddError-2"] = "URL Không đúng";
+$string["songAddError-3"] = "Bài hát này đã được tải lại";
+$string["songAddError-4"] = "Bài hát này không tải lại được";
+
+/*
+	STATS
+*/
+$string["ID"] = "ID";
+$string["stars"] = "Stars";
+$string["coins"] = "Sao";
+$string["accounts"] = "Tài khoản";
+$string["levels"] = "Levels";
+$string["songs"] = "Nhạc";
+$string["author"] = "Người sáng tạo";
+$string["name"] = "Tên";
+$string["userCoins"] = "Tiền xu của người dùng";
+$string["time"] = "Thời gian";
+$string["deletedLevel"] = "Level đã bị xóa";
+$string["mod"] = "Người điều hành";
+$string["count"] = "Số tiền";
+$string["ratedLevels"] = "Xếp hạng Levels";
+$string["lastSeen"] = "Thời gian qua trực tuyến";
+$string["level"] = "Level";
+$string["pageInfo"] = "Trang %s trên %s";
+$string["first"] = "Đầu tiên";
+$string["previous"] = "Trước";
+$string["next"] = "Kế tiếp";
+$string["last"] = "Cuối cùng";
+//modActionsList
+$string["action"] = "Action";
+$string["value"] = "1st Value";
+$string["value2"] = "2nd Value";
+$string["modAction1"] = "Rated a level";
+$string["modAction2"] = "Un/featured a level";
+$string["modAction3"] = "Un/verified coins";
+$string["modAction4"] = "Un/epiced level";
+$string["modAction5"] = "Đặt làm Level hàng ngày";
+$string["modAction6"] = "Xóa level";
+$string["modAction7"] = "Thay đổi người tạo Levels";
+$string["modAction8"] = "Đổi tên level";
+$string["modAction9"] = "Thay đổi mật khẩu level";
+$string["modAction10"] = "Thay đổi Cấp độ Demon";
+$string["modAction11"] = "Đã chia sẻ CP";
+$string["modAction12"] = "Un/pub lished";
+$string["modAction13"] = "Mô tả Levels thay đổi";
+$string["modAction14"] = "Bật/Tắt LDM";
+$string["modAction15"] = "Leaderboard un/ban";
+//errors
+$string["errorNoAccWithPerm"] = "Lỗi: Không tìm thấy tài khoản nào có quyền '%s'";
