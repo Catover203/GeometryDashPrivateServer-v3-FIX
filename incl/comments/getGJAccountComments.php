2403a0599ea1eac48c20f96a54edbce35882cdd7
Catover203 Creator <63941519+Catover203@users.noreply.github.com>
2020-05-09 17:12:31 +0700

    Update getGJAccountComments.php

--- incl/comments/getGJAccountComments.php
+++ incl/comments/getGJAccountComments.php
@@ -1,36 +1,33 @@
 <?php
-//Requesting files
 chdir(dirname(__FILE__));
+//error_reporting(0);
 include "../lib/connection.php";
 require_once "../lib/exploitPatch.php";
-require_once "../lib/mainLib.php";
 $ep = new exploitPatch();
+require_once "../lib/mainLib.php";
 $gs = new mainLib();
-//Checking secret
-if($ep->remove($_POST["secret"]) != "Wmfd2893gb7") exit("#0:0:0");
-//Getting post data
-$commentsString = "";
+$commentstring = "";
+$accountid = $ep->remove($_POST["accountID"]);
 $page = $ep->remove($_POST["page"]);
-$accountID = $ep->remove($_POST["accountID"]);
-$commentPage = $page * 10;
-//Getting account comments
-$query = $db->prepare("SELECT * FROM acccomments WHERE auserID = :uid ORDER BY timestamp DESC LIMIT 10 OFFSET $commentPage");
-$query->execute([':accountID' => $accountID]);
-$comments = $query->fetchAll();
-//Counting account comments
-if(!$query->rowCount()) exit("#0:0:0");
-$query = $db->prepare("SELECT count(*) FROM accComments WHERE accountID = :accountID");
-$query->execute([':uid' => $accountID]);
-$commentCount = $query->fetchColumn();
-//Fetching account comments
-foreach($comments as &$comment) {
-	if($comment["commentID"]){
-		$uploadDate = $gs->convertDate(date("Y-m-d H:i:s", $comment["timestamp"]));
-		$commentsString .= "2~".$comment["comment"]."~4~".$comment["likes"]."~9~".$uploadDate."~6~".$comment["commentID"]."|";
+$commentpage = $page*10;
+$userID = $gs->getUserID($accountid);
+$query = "SELECT comment, userID, likes, isSpam, commentID, timestamp FROM acccomments WHERE userID = :userID ORDER BY timeStamp DESC LIMIT 10 OFFSET $commentpage";
+$query = $db->prepare($query);
+$query->execute([':userID' => $userID]);
+$result = $query->fetchAll();
+if($query->rowCount() == 0){
+	exit("#0:0:0");
+}
+$countquery = $db->prepare("SELECT count(*) FROM acccomments WHERE userID = :userID");
+$countquery->execute([':userID' => $userID]);
+$commentcount = $countquery->fetchColumn();
+foreach($result as &$comment1) {
+	if($comment1["commentID"]!=""){
+		$uploadDate = date("d/m/Y G:i", $comment1["timestamp"]);
+		$commentstring .= "2~".$comment1["comment"]."~3~".$comment1["userID"]."~4~".$comment1["likes"]."~5~0~7~".$comment1["isSpam"]."~9~".$uploadDate."~6~".$comment1["commentID"]."|";
 	}
 }
-//Printing account comments
-$commentsString = substr($commentsString, 0, -1);
-echo $commentsString;
-echo "#".$commentCount.":".$commentPage.":10";
+$commentstring = substr($commentstring, 0, -1);
+echo $commentstring;
+echo "#".$commentcount.":".$commentpage.":10";
 ?>
