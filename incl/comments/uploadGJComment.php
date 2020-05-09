b66f4f2f7966395ff97592fc837181ca1cde8186
Catover203 Creator <63941519+Catover203@users.noreply.github.com>
2020-05-09 17:13:19 +0700

    Update uploadGJComment.php

--- incl/comments/uploadGJComment.php
+++ incl/comments/uploadGJComment.php
@@ -3,49 +3,30 @@
 chdir(dirname(__FILE__));
 include "../lib/connection.php";
 require_once "../lib/mainLib.php";
-require_once "../lib/mainLib.php";
 require_once "../lib/GJPCheck.php";
-require_once "../lib/XORCipher.php";
 require_once "../misc/commands.php";
 require_once "../lib/exploitPatch.php";
 $gs = new mainLib();
 $cmds = new Commands();
-$mainLib = new mainLib();
 $ep = new exploitPatch();
 $GJPCheck = new GJPCheck();
 //Getting data
-if($ep->remove($_POST["secret"]) != "Wmfd2893gb7") exit("-1");
 $gjp = $ep->remove($_POST["gjp"]);
 $comment = $ep->remove($_POST["comment"]);
-$levelID = $ep->remove($_POST["levelID"]);
 $username = $ep->remove($_POST["userName"]);
-$gameVersion = $ep->remove($_POST["gameVersion"]);
-$binaryVersion = $ep->remove($_POST["binaryVersion"]);
-//Checking percent
-if(!empty($_POST["percent"])){
-	$percent = $ep->remove($_POST["percent"]);
-}else{
-	$percent = 0;
-}
-//Checking if registered
-if(!empty($_POST["accountID"]) && $_POST["accountID"] && is_numeric($_POST["accountID"])){
-	$accountID = $ep->remove($_POST["accountID"]);
-	//Checking GJP
-	if(!$GJPCheck->check($gjp, $accountID)) exit("-1");
-}else{
-	//Not registered
-	exit("-1");
-}
-if($cmds->doCommands($accountID, base64_decode($comment), $levelID)) exit("-1");
+$accountID = $ep->remove($_POST["accountID"]);
+if($ep->remove($_POST["secret"]) != "Wmfd2893gb7") exit("-1");
 //Checking if banned
 if($gs->isBanned($accountID, "comment")) exit("-10");
-if($accountID && is_numeric($accountID) && $comment){
+//User check
+if($accountID != "" && is_numeric($accountID) && $comment != "" && $GJPCheck->check($gjp, $accountID)){
+	//Command
+	if($cmds->doProfileCommands($accountID, base64_decode($comment))) exit("-1");
 	//Commenting
-	$query = $db->prepare("INSERT INTO comments (username, comment, levelID, accountID, timestamp, percent) VALUES (:username, :comment, :levelID, :accountID, :uploadDate, :percent)");
-	$query->execute([':username' => $username, ':comment' => $comment, ':levelID' => $levelID, ':accountID' => $accountID, ':uploadDate' => time(), ':percent' => $percent]);
+	$query = $db->prepare("INSERT INTO accComments (username, comment, accountID, timestamp)
+										VALUES (:username, :comment, :accountID, :uploadDate)");
+	$query->execute([':username' => $username, ':comment' => $comment, ':accountID' => $accountID, ':uploadDate' => time()]);
 	echo 1;
 }else{
 	//Failure
 	exit("-1");
-}
-?>
