936d91d9f8dcd86216dc9a96605e0b0ea56ef9b6
Catover203 Creator <63941519+Catover203@users.noreply.github.com>
2020-05-24 17:13:48 +0700

    Update changeUsername.php

--- dashboard/account/changeUsername.php
+++ dashboard/account/changeUsername.php
@@ -3,12 +3,12 @@
 session_start();
 if(!isset($_SESSION["accountID"]) || !$_SESSION["accountID"]) exit(header("Location: ../login/login.php"));
 //Requesting files
+include "../../config/email.php";
 include "../../incl/lib/connection.php";
 require_once "../incl/dashboardLib.php";
 require_once "../../incl/lib/mainLib.php";
 require_once "../../incl/lib/exploitPatch.php";
 require_once "../../incl/lib/generatePass.php";
-$qna = 0;
 $gs = new mainLib();
 $ep = new exploitPatch();
 $dl = new dashboardLib();
@@ -23,7 +23,7 @@ if(isset($_GET["e"]) && isset($_GET["u"]) && isset($_GET["p"]) && isset($_GET["n
 	//Checking password
 	if($generatePass->isValidUsrname($username, $password)){
 		//Checking if username exists
-		$query = $db->prepare("SELECT username FROM accounts WHERE username = :username LIMIT 1");
+		$query = $db->prepare("SELECT userName FROM accounts WHERE userName = :username LIMIT 1");
 		$query->execute([':username' => $newUsername]);
 		if($query->rowCount() > 0){
 			//Printing error
@@ -33,17 +33,17 @@ if(isset($_GET["e"]) && isset($_GET["u"]) && isset($_GET["p"]) && isset($_GET["n
 							<a class='btn btn-primary btn-block' href='".$_SERVER["REQUEST_URI"]."'>".$dl->getLocalizedString("tryAgainBTN")."</a>","account"));
 		}
 		//Updating username
-		$query = $db->prepare("UPDATE acccomments SET username = :newUsername WHERE username = :username");
+		$query = $db->prepare("UPDATE acccomments SET userName = :newUsername WHERE userName = :username");
 		$query->execute([':newUsername' => $newUsername, ':username' => $username]);
-		$query = $db->prepare("UPDATE comments SET username = :newUsername WHERE username = :username");
+		$query = $db->prepare("UPDATE comments SET userName = :newUsername WHERE userName = :username");
 		$query->execute([':newUsername' => $newUsername, ':username' => $username]);
-		$query = $db->prepare("UPDATE levels SET username = :newUsername WHERE username = :username");
+		$query = $db->prepare("UPDATE levels SET userName = :newUsername WHERE username = :userName");
 		$query->execute([':newUsername' => $newUsername, ':username' => $username]);
-		$query = $db->prepare("UPDATE messages SET username = :newUsername WHERE username = :username");
+		$query = $db->prepare("UPDATE messages SET userName = :newUsername WHERE userName = :username");
 		$query->execute([':newUsername' => $newUsername, ':username' => $username]);
-		$query = $db->prepare("UPDATE users SET username = :newUsername WHERE username = :username");
+		$query = $db->prepare("UPDATE users SET userName = :newUsername WHERE userName = :username");
 		$query->execute([':newUsername' => $newUsername, ':username' => $username]);
-		$query = $db->prepare("UPDATE accounts SET username= :newUsername WHERE username = :username AND email = :email");	
+		$query = $db->prepare("UPDATE accounts SET userName= :newUsername WHERE userName = :username AND email = :email");	
 		$query->execute([':newUsername' => $newUsername, ':username' => $username, ':email' => $email]);
 		if($query->rowCount() == 0){
 			//Printing error
@@ -52,9 +52,9 @@ if(isset($_GET["e"]) && isset($_GET["u"]) && isset($_GET["p"]) && isset($_GET["n
 							<p>$errorDesc</p>
 							<a class='btn btn-primary btn-block' href='".$_SERVER["REQUEST_URI"]."'>".$dl->getLocalizedString("tryAgainBTN")."</a>","account"));
 		}else{
-			$query = $db->prepare("UPDATE accounts SET usernameCount = usernameCount + 1 WHERE username= :username AND email = :email");
+			$query = $db->prepare("UPDATE accounts SET usernameCount = usernameCount + 1 WHERE userName= :username AND email = :email");
 			$query->execute([':username' => $newUsername, ':email' => $email]);
-			$query = $db->prepare("UPDATE accounts SET usernameChangeDate = :timestamp WHERE username = :username AND email = :email");
+			$query = $db->prepare("UPDATE accounts SET usernameChangeDate = :timestamp WHERE userName = :username AND email = :email");
 			$query->execute([':timestamp' => time(), ':username' => $newUsername, ':email' => $email]);
 			$dl->printBox("<h1>".$dl->getLocalizedString("changeUsername")."</h1>
 			<p>".sprintf($dl->getLocalizedString("usernameChanged"), $newUsername)."</p>","account");
@@ -76,23 +76,23 @@ if(isset($_POST["username"]) && isset($_POST["newUsername"]) && isset($_POST["pa
 	$baseNewUsername = base64_encode($newUsername);
 	$password = $ep->remove($_POST["password"]);
 	$basePassword = base64_encode($password);
-	$query = $db->prepare("SELECT email FROM accounts WHERE username = :username LIMIT 1");
+	$query = $db->prepare("SELECT email FROM accounts WHERE userName = :username LIMIT 1");
 	$query->execute([':username' => $username]);
 	$email = $query->fetchColumn();
 	$baseEmail = base64_encode($email);
 	//Checking pass
 	if ($generatePass->isValidUsrname($username, $password)) {
 		//Checking how many times username has been changed
-		$query = $db->prepare("SELECT usernameCount FROM accounts WHERE username = :username AND email = :email LIMIT 1");
+		$query = $db->prepare("SELECT usernameCount FROM accounts WHERE userName = :username AND email = :email LIMIT 1");
 		$query->execute([':username' => $username, ':email' => $email]);
 		if($query->fetchColumn() > 2){
-			$errorDesc = $dl->getLocalizedString("You can't change password more than 2 time");
+			$errorDesc = $dl->getLocalizedString("changeUsernameError-4");
 			exit($dl->printBox('<h1>'.$dl->getLocalizedString("changeUsername")."</h1>
 							<p>$errorDesc</p>
 							<a class='btn btn-primary btn-block' href='".$_SERVER["REQUEST_URI"]."'>".$dl->getLocalizedString("tryAgainBTN")."</a>","account"));
 		}
 		//Checking when was the last username change
-		$query = $db->prepare("SELECT usernameChangeDate FROM accounts WHERE username = :username AND email = :email LIMIT 1");
+		$query = $db->prepare("SELECT userNameChangeDate FROM accounts WHERE userName = :username AND email = :email LIMIT 1");
 		$query->execute([':username' => $username, ':email' => $email]);
 		$lastChange = $query->fetchColumn();
 		$timestamp = time() - 2592000;
@@ -105,7 +105,7 @@ if(isset($_POST["username"]) && isset($_POST["newUsername"]) && isset($_POST["pa
 							<a class='btn btn-primary btn-block' href='".$_SERVER["REQUEST_URI"]."'>".$dl->getLocalizedString("tryAgainBTN")."</a>","account"));
 		}
 		//Checking if username exists
-		$query = $db->prepare("SELECT username FROM accounts WHERE username = :username LIMIT 1");
+		$query = $db->prepare("SELECT userName FROM accounts WHERE userName = :username LIMIT 1");
 		$query->execute([':username' => $newUsername]);
 		if($query->rowCount() > 0){
 			//Printing error
@@ -114,7 +114,7 @@ if(isset($_POST["username"]) && isset($_POST["newUsername"]) && isset($_POST["pa
 							<p>$errorDesc</p>
 							<a class='btn btn-primary btn-block' href='".$_SERVER["REQUEST_URI"]."'>".$dl->getLocalizedString("tryAgainBTN")."</a>","account"));
 		}
-		if($qna){
+		if($emailEnabled){
 			//Sending email
 			$URI = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 			$body = dirname($URI)."/dashboard/account/changeUsername.php?u=$baseUsername&e=$baseEmail&n=$baseNewUsername&p=$basePassword.";
@@ -129,23 +129,23 @@ if(isset($_POST["username"]) && isset($_POST["newUsername"]) && isset($_POST["pa
 				$dl->printBox("<h1>".$dl->getLocalizedString("changeUsername")."</h1>
 					<p>".$dl->getLocalizedString("emailSended")."</p>","account");
 			}
-		}elseif(!$qna){
+		}elseif(!$emailEnabled){
 			//Updating username
-			$query = $db->prepare("UPDATE accComments SET username = :newUsername WHERE username = :username");
+			$query = $db->prepare("UPDATE accComments SET userName = :newUsername WHERE userName = :username");
 			$query->execute([':newUsername' => $newUsername, ':username' => $username]);
-			$query = $db->prepare("UPDATE comments SET username = :newUsername WHERE username = :username");
+			$query = $db->prepare("UPDATE comments SET userName = :newUsername WHERE userName = :username");
 			$query->execute([':newUsername' => $newUsername, ':username' => $username]);
-			$query = $db->prepare("UPDATE levels SET username = :newUsername WHERE username = :username");
+			$query = $db->prepare("UPDATE levels SET userName = :newUsername WHERE userName = :username");
 			$query->execute([':newUsername' => $newUsername, ':username' => $username]);
-			$query = $db->prepare("UPDATE messages SET username = :newUsername WHERE username = :username");
+			$query = $db->prepare("UPDATE messages SET userName = :newUsername WHERE userName = :username");
 			$query->execute([':newUsername' => $newUsername, ':username' => $username]);
-			$query = $db->prepare("UPDATE users SET username = :newUsername WHERE username = :username");
-			$query->execute([':newUsername' => $newUsername, ':username' => $username]);
-			$query = $db->prepare("UPDATE accounts SET username = :newUsername WHERE username = :username AND email = :email");	
+			$query = $db->prepare("UPDATE users SET userName = :newUsername WHERE userName = :username");
+			$query->execute([':newUsername' => $newUserame, ':username' => $username]);
+			$query = $db->prepare("UPDATE accounts SET userName = :newUsername WHERE username = :username AND email = :email");	
 			$query->execute([':newUsername' => $newUsername, ':username' => $username, ':email' => $email]);
 			if(!$query->rowCount()){
 				//Printing error
-				$errorDesc = $dl->getLocalizedString("change Username Error -1");
+				$errorDesc = $dl->getLocalizedString("changeUsernameError-1");
 				exit($dl->printBox('<h1>'.$dl->getLocalizedString("changeUsername")."</h1>
 								<p>$errorDesc</p>
 								<a class='btn btn-primary btn-block' href='".$_SERVER["REQUEST_URI"]."'>".$dl->getLocalizedString("tryAgainBTN")."</a>","account"));
@@ -170,11 +170,11 @@ if(isset($_POST["username"]) && isset($_POST["newUsername"]) && isset($_POST["pa
 	$dl->printBox('<h1>'.$dl->getLocalizedString("changeUsername").'</h1>
 				<form action="" method="post">
 					<div class="form-group">
-						<input type="text" class="form-control" id="changeUsernameUserName" name="username" placeholder="UserName"><br>
-						<input type="text" class="form-control" id="changeUsernameNewUserName" name="newUsername" placeholder="New UserName"><br>
-						<input type="password" class="form-control" id="changeUsernamePassword" name="password" placeholder="Password">
+						<input type="text" class="form-control" id="changeUsernameUserName" name="username" placeholder="'.$dl->getLocalizedString("changeUsernameUserNameFieldPlaceholder").'"><br>
+						<input type="text" class="form-control" id="changeUsernameNewUserName" name="newUsername" placeholder="'.$dl->getLocalizedString("changeUsernameNewUserFieldPlaceholder").'"><br>
+						<input type="password" class="form-control" id="changeUsernamePassword" name="password" placeholder="'.$dl->getLocalizedString("changeUsernamePasswordFieldPlaceholder").'">
 					</div>
-					<button type="submit" class="btn btn-primary btn-block">Change Username</button>
+					<button type="submit" class="btn btn-primary btn-block">'.$dl->getLocalizedString("changeBTN").'</button>
 				</form>',"account");
 }
 ?>
