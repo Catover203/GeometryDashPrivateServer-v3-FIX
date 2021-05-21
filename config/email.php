465d241a1bf4122d7a71bc8308dfee440a9ced2d
Catover203 Creator <63941519+Catover203@users.noreply.github.com>
2021-05-21 23:55:30 +0700

    Add files via upload

--- config/email.php
+++ config/email.php
@@ -1,7 +1,9 @@
 <?php
-    $emailMail = "";
-    $emailUsername = "";
-    $emailPassword = "";
-    $emailHost = "";
+    $emailMail = "someone@domain.com";
+    $emailUsername = "someone@domain.com";
+    $emailPassword = "Password123456";
+    $emailHost = "smtp.domain.com";
+    $emailPort = 587;
+    $emailSecure = "tls";
     $emailEnabled = 0;
 ?>
